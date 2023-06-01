b357be7418bc08196d32197604b4e2efe7a4c13f
sebaactis <sebaactis@gmail.com>
2023-06-01 13:47:20 -0300

    merge seba

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,15 +3,6 @@ import React from 'react'
 const NavBar = () => {
     return (
         <>
-            {/*  <ul className="navBar">
-            <img className="logo" src='../src/multimedy/logo.png' /> 
-                <li>HOME</li>
-                <li>CONVOCATORIA</li>
-                <li>NOTICIAS</li>
-                <li>SOBRE NOSOTROS</li>
-                <li>TRAYECTORIA</li>
-                <li>CONTACTO</li>
-            </ul> */}
             <nav className="navBarStyle navbar navbar-expand-lg">
                 <img className="logo" src='../src/multimedy/logo.png' />
                 <div className="container-fluid">
