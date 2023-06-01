a21811837efcef1b08565dbbab6f2d3161d99f64
sebaactis <sebaactis@gmail.com>
2023-06-01 11:36:57 -0300

    navbar y home v1

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <>
+            <ul className="navBar">
+            <img className="logo" src='../src/multimedy/logo.png' /> 
+                <li>HOME</li>
+                <li>CONVOCATORIA</li>
+                <li>NOTICIAS</li>
+                <li>SOBRE NOSOTROS</li>
+                <li>TRAYECTORIA</li>
+                <li>CONTACTO</li>
+            </ul>
+        </>
+    )
+}
+
+export default NavBar
