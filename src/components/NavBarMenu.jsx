11b6c6dc6e87b703f3a532e4759bcef42f7fb20a
sebaactis <sebaactis@gmail.com>
2023-07-03 12:38:03 -0300

    prueba popovers ocultos movil

--- src/components/NavBarMenu.jsx
+++ src/components/NavBarMenu.jsx
@@ -7,9 +7,12 @@ import sobreNosotros from "../videos/SobreNosotros.webm";
 import trayectoria from "../videos/Trayectoria.webm";
 import contacto from "../videos/Contacto.webm";
 import { useAccContext } from "../context/AccContext";
+import { useMediaQuery } from "react-responsive";
 
 function NavBarMenu() {
     const { incSize, sizeBtn } = useAccContext();
+    const isMobile = useMediaQuery({ query: "(max-width: 987px)" })
+
 
     const menuArray = [
         { title: "HOME", url: home, href: "/" },
@@ -42,29 +45,46 @@ function NavBarMenu() {
 
     // <iframe src="https://drive.google.com/file/d/1hbB63Ehk5yc1YwuCg8XOkrt2lAJdHleR/preview" width="640" height="480" allow="autoplay"></iframe>
 
-    return (
-        <ul className="navbar-nav gap-1" id="navbar-nav">
-            {menuArray.map((menu) => (
-                <li className="nav-item" key={menu.title}>
-                    <OverlayTrigger
-                        trigger="hover"
-                        key={menu.title}
-                        placement="bottom"
-                        overlay={
-                            <Popover id={menu.title}>
-                                <video className="popoverVideo" src={menu.url} width="200" autoPlay></video>
-                            </Popover>
-                        }
-                        className="videoNavBar"
-                    >
+
+
+    if (isMobile) {
+        return (
+            <ul className="navbar-nav gap-1" id="navbar-nav" >
+                {menuArray.map((menu) => (
+
+                    <li className="nav-item" key={menu.title}>
                         <a className={`nav-link ${incSize ? "fs-5" : ""}`} href={menu.href}>
                             {menu.title}
                         </a>
-                    </OverlayTrigger>
-                </li>
-            ))}
-        </ul>
-    );
-}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
 
+    return (
+        <ul className="navbar-nav gap-1" id="navbar-nav" >
+            {
+                menuArray.map((menu) => (
+                    <li className="nav-item" key={menu.title}>
+                        <OverlayTrigger
+                            trigger="hover"
+                            key={menu.title}
+                            placement="bottom"
+                            overlay={
+                                <Popover id={menu.title}>
+                                    <video src={menu.url} width="200" autoPlay></video>
+                                </Popover>
+                            }
+                        >
+                            <a className={`nav-link ${incSize ? "fs-5" : ""}`} href={menu.href}>
+                                {menu.title}
+                            </a>
+                        </OverlayTrigger>
+                    </li>
+                ))
+            }
+        </ul>
+    )
+}
 export default NavBarMenu;
